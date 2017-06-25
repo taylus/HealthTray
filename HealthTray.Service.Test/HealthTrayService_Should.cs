31d8168f6b1e18ebddefe78dc0c9a5ef6e1403e0
Taylus <dragondude@gmail.com>
2017-06-24 22:39:59 -0500

    Adding healthchecks REST API client; reorganizing projects

---
+++ HealthTray.Service.Test/HealthTrayService_Should.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HealthTray.Service.Test
+{
+    [TestClass]
+    public class HealthTrayService_Should
+    {
+        [TestMethod]
+        public async Task Return_Checks()
+        {
+            //arrange - set up a successful response w/ check data in the expected JSON format
+            var stubHandler = new StubHttpClientHandler();
+            stubHandler.EnqueueResponse(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("{ \"checks\": [ { \"last_ping\": \"2017-01-04T13:24:39.903464+00:00\", \"ping_url\": \"https://hchk.io/662ebe36-ecab-48db-afe3-e20029cb71e6\", \"next_ping\": \"2017-01-04T14:24:39.903464+00:00\", \"grace\": 900, \"name\": \"Api test 1\", \"n_pings\": 1, \"tags\": \"foo\", \"pause_url\": \"https://healthchecks.io/api/v1/checks/662ebe36-ecab-48db-afe3-e20029cb71e6/pause\", \"timeout\": 3600, \"status\": \"up\", \"update_url\": \"https://healthchecks.io/api/v1/checks/662ebe36-ecab-48db-afe3-e20029cb71e6\" }, { \"last_ping\": null, \"ping_url\": \"https://hchk.io/9d17c61f-5c4f-4cab-b517-11e6b2679ced\", \"next_ping\": null, \"grace\": 3600, \"name\": \"Api test 2\", \"n_pings\": 0, \"tags\": \"bar baz\", \"pause_url\": \"https://healthchecks.io/api/v1/checks/9d17c61f-5c4f-4cab-b517-11e6b2679ced/pause\", \"tz\": \"UTC\", \"schedule\": \"0/10 * * * *\", \"status\": \"new\", \"update_url\": \"https://healthchecks.io/api/v1/checks/9d17c61f-5c4f-4cab-b517-11e6b2679ced\" } ] }")
+            });
+
+            //act
+            var service = new HealthTrayService(new HttpClient(stubHandler));
+            var checks = await service.GetChecks();
+
+            //assert
+            Assert.AreEqual(2, checks.Count);
+        }
+
+        [TestMethod, ExpectedException(typeof(HttpRequestException))]
+        public async Task Throw_Exception_On_Unsuccessful_Response()
+        {
+            //arrange - set up a successful response w/ check data in the expected JSON format
+            var stubHandler = new StubHttpClientHandler();
+            stubHandler.EnqueueResponse(new HttpResponseMessage(HttpStatusCode.InternalServerError));
+
+            //act
+            var service = new HealthTrayService(new HttpClient(stubHandler));
+            await service.GetChecks();
+        }
+    }
+}
---
+++ HealthTray.Service.Test/StubHttpClientHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace HealthTray.Service.Test
+{
+    /// <summary>
+    /// A test stand-in for <see cref="HttpClientHandler"/>, the default message
+    /// handler which normally sends requests out to the network. <para/>
+    /// 
+    /// This handler maintains a queue of responses and sends them back in order.
+    /// It is intended to be used in unit tests, where we don't want to hit actual
+    /// services over HTTP.
+    /// </summary>
+    public class StubHttpClientHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Internal queue of responses returned by this handler.
+        /// </summary>
+        private readonly Queue<HttpResponseMessage> responses = new Queue<HttpResponseMessage>();
+
+        /// <summary>
+        /// Queues up the given response to be returned from this handler's SendAsync.
+        /// </summary>
+        public void EnqueueResponse(HttpResponseMessage response)
+        {
+            responses.Enqueue(response);
+        }
+
+        /// <summary>
+        /// Processes the given request through the message handler pipeline.
+        /// </summary>
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (responses.Count == 0) throw new InvalidOperationException("No responses are queued for this request.");
+            return Task.FromResult(responses.Dequeue());
+        }
+    }
+}
