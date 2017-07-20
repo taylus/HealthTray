b650d2c9331d40516661161cb2b6e340d1548925
taylus <therealbjohnson@gmail.com>
2017-07-20 15:56:04 -0500

    Test the dashboard's UI with a stub service

--- HealthTray.Wpf/App.xaml.cs
+++ HealthTray.Wpf/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Media;
+using System.Collections.Generic;
 using Hardcodet.Wpf.TaskbarNotification;
 using HealthTray.Service;
+using HealthTray.Service.Model;
 
 namespace HealthTray.Wpf
 {
@@ -14,8 +17,33 @@ namespace HealthTray.Wpf
             base.OnStartup(e);
             tb = (TaskbarIcon)FindResource("TaskbarIcon"); //taskbar icon stays in the system tray until disposed/application exits
 
-            //TODO: read API key and URL from config + make a settings window
-            var service = new HealthTrayService("https://healthchecks.io/api/v1/", "");
+            //TODO: use real API, read API key and URL from config, and make a settings window for the user to specify them
+            //var service = new HealthTrayService("https://healthchecks.io/api/v1/", "");
+            var service = new StubHealthTrayService(new List<Check>
+            {
+                new Check()
+                {
+                    name = "A test check",
+                    last_ping = DateTime.Now.AddMinutes(-1.5)
+                },
+                new Check()
+                {
+                    name = "Another test check",
+                    last_ping = DateTime.Now
+                },
+                new Check()
+                {
+                    name = "Yet another test check",
+                    last_ping = DateTime.Now.AddHours(-30.5),
+                    status = CheckStatus.late
+                },
+                new Check()
+                {
+                    name = "Oh no it's down",
+                    last_ping = DateTime.Now.AddDays(-10),
+                    status = CheckStatus.down
+                },
+            });
             var dashboard = new DashboardWindow(service);
             dashboard.Show();
         }
---
+++ HealthTray.Wpf/Testing/StubHealthTrayService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using HealthTray.Service.Model;
+
+namespace HealthTray.Service
+{
+    public class StubHealthTrayService : IHealthTrayService
+    {
+        public IList<Check> Checks { get; private set; }
+
+        public StubHealthTrayService(IList<Check> checks)
+        {
+            Checks = checks;
+        }
+
+        public Task<IList<Check>> GetChecks()
+        {
+            return Task.FromResult(Checks);
+        }
+    }
+}
