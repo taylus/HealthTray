97712fe0db1409ff2da3b013d0bf3b5c2891764b
Taylus <dragondude@gmail.com>
2017-07-20 21:39:59 -0500

    Load settings from config file

--- HealthTray.Wpf/SettingsControl.xaml.cs
+++ HealthTray.Wpf/SettingsControl.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Diagnostics;
+using System.Configuration;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 
@@ -12,12 +11,27 @@ namespace HealthTray.Wpf
         public SettingsControl()
         {
             InitializeComponent();
+            LoadSettings();
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(e.Uri.AbsoluteUri);
             e.Handled = true;
+        }
+
+        private void LoadSettings()
+        {
+            string apiDocsUrl = ConfigurationManager.AppSettings["healthchecks-api-docs"];
+            Uri apiDocsUri;
+            if(Uri.TryCreate(apiDocsUrl, UriKind.Absolute, out apiDocsUri))
+            {
+                apiDocsLink.NavigateUri = apiDocsUri;
+            }
+
+            apiUrl.Text = ConfigurationManager.AppSettings["healthchecks-api-url"];
+            apiKey.Text = ConfigurationManager.AppSettings["healthchecks-api-key"];
+            refreshSeconds.Text = ConfigurationManager.AppSettings["refresh-seconds"];
         }
     }
 }
