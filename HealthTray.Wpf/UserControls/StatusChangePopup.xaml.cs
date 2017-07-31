4fd27ba9bb2e79fd8b128f47b8bc97705a29f2ec
taylus <therealbjohnson@gmail.com>
2017-07-31 13:47:51 -0500

    Clicking a status change popup shows the dashboard

--- HealthTray.Wpf/UserControls/StatusChangePopup.xaml.cs
+++ HealthTray.Wpf/UserControls/StatusChangePopup.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Media;
+using System.Windows.Input;
 using System.Windows.Controls;
 
 namespace HealthTray.Wpf
@@ -8,6 +9,12 @@ namespace HealthTray.Wpf
         public StatusChangePopup()
         {
             InitializeComponent();
+            MouseLeftButtonUp += ShowDashboard_ClickHandler;
+        }
+
+        private void ShowDashboard_ClickHandler(object sender, MouseButtonEventArgs e)
+        {
+            new ShowDashboardCommand().Execute(null);
         }
 
         public StatusChangePopup(ImageSource image, string statusText) : this()
